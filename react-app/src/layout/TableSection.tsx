ab79c787dab8abafd878582ca7d426d0471cb3df
Adelya <msvadelya@gmail.com>
2025-06-08 19:56:47 +0200

    added mui table

--- react-app/src/layout/TableSection.tsx
+++ react-app/src/layout/TableSection.tsx
@@ -1,11 +1,12 @@
 import SearchField from "../components/SearchField";
 import AddIcon from "@mui/icons-material/Add";
+import ExpensesTable from "../components/ExpensesTable";
 
 function TableSection() {
   return (
-    <section className="bg-white shadow-xl min-h-[50vh] my-10 p-5">
+    <section className="bg-white shadow-xl min-h-[50vh] my-10 p-9 rounded-3xl">
       <p className="text-2xl  font-bold mb-5">Income and Expenses</p>
-      <div className="flex justify-between ">
+      <div className="flex justify-between  mb-7">
         <SearchField />
         <div className=" flex items-center gap-2">
           <button className="text-white bg-black py-2 px-4 rounded-2xl font-bold">
@@ -18,6 +19,7 @@ function TableSection() {
           </button>
         </div>
       </div>
+      <ExpensesTable/>
     </section>
   );
 }
